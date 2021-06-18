c9a71cf697e6debde7871cdac11b5736e0a1edfe
saito <saito@asterisoft.co.jp>
2021-06-18 22:33:08 +0900

    Added init type to create app directory

--- susvm.py
+++ susvm.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 import subprocess
 
@@ -7,14 +8,21 @@ import subprocess
 PROJECT_NAME = 'susvm.py'
 PROJECT_PATH = r'C:\Users\Flat\Desktop\susvm'
 
+APP_PATH = r'C:\Users\Flat\Desktop\SUSPlayer2'
+APP_MASTER_PATH = rf'{APP_PATH}\master'
+APP_VERSIONS_PATH = rf'{APP_PATH}\versions'
+_APP_TMP_PATH = rf'{APP_PATH}\tmp'
+
+TYPE_INIT = 'init'
 TYPE_BUILD = 'build'
+TYPES = [TYPE_INIT, TYPE_BUILD]
 
 
 
 # Arguments
 parser = argparse.ArgumentParser()
 
-parser.add_argument('type', choices = [TYPE_BUILD])
+parser.add_argument('type', choices = TYPES)
 
 args = parser.parse_args()
 
@@ -25,10 +33,21 @@ args = parser.parse_args()
 
 
 # Core
+def init():
+    if not os.path.exists(APP_PATH):
+        os.mkdir(APP_PATH)
+
+    if not os.path.exists(APP_MASTER_PATH):
+        os.mkdir(APP_MASTER_PATH)
+
+    if not os.path.exists(APP_VERSIONS_PATH):
+        os.mkdir(APP_VERSIONS_PATH)
+
 def build():
     subprocess.call(f'pyinstaller {PROJECT_PATH}\\{PROJECT_NAME} --onefile')
 
 
 
 # Types
-if args.type == TYPE_BUILD: build()
+if args.type == TYPE_INIT: init()
+elif args.type == TYPE_BUILD: build()
